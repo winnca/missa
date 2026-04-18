7c917f574ae603957e966b51174cb8f423fd2531
Alexandr <114196423+winnca@users.noreply.github.com>
2026-04-18 17:27:46 +0300

    Delete src/practice_2 directory

--- src/practice_2/task_2_2/PaymentMethod.java
+++
@@ -1,8 +0,0 @@
-package practice_2.task_2_2;
-
-public sealed interface PaymentMethod permits CreditCard, BankTransfer, CryptoWallet {
-
-    String process(double amount);
-
-    double fee(double amount);
-}
