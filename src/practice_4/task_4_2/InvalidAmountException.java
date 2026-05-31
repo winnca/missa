e170ad377bec244a4e9dc1ffe7b42cbc13e38301
Alexandr <114196423+winnca@users.noreply.github.com>
2026-05-31 22:45:29 +0300

    Delete src/practice_4 directory

--- src/practice_4/task_4_2/InvalidAmountException.java
+++
@@ -1,7 +0,0 @@
-package practice_4.task_4_2;
-
-public class InvalidAmountException extends RuntimeException{
-    public InvalidAmountException(double amount) {
-        super("Сумма должна быть положительной, получено: " + amount);
-    }
-}
